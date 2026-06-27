9b3dd6f4ca5210245e98df18ffeae25d738840a7
mabrisenom-spec <mabrisenom@estudiantes.uhemisferios.edu.ec>
2026-06-27 08:59:47 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
 import streamlit as st
 
-st.title("Aplicación de Fundamentos Streamlit")
-st.sidebar.title("Parámetros")
+st.title("Aplicación de Fundamentos Streamlit
 
 st.sidebar.image("images.jpg")
+
+st.sidebar.title("Parámetros")
+
+monto = st.number_input("Ingrese el Monto de su Prestamo"
+ 
+interes = st.number_input("Ingrese el interes")
+ 
+tiempo_meses = st.number_input("Ingrese el tiempo en meses")
+ 
+resultado = monto*interes*(tiempo_meses/12)    
+ 
+st.write("El resultado es:" , resultado)
